49d067188485b9a1cae0a7727708bd434105e298
danjuan-77 <youngertutu@gmail.com>
2025-08-24 16:02:35 +0800

    update eval scripts

--- scripts/vocalnet-asr-eval-qwen7b-sft-3.sh
+++ scripts/vocalnet-asr-eval-qwen7b-sft-3.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=3
 export HF_ENDPOINT=https://hf-mirror.com
 
 # VocalNet模型路径配置
-export VOCALNET_MODEL="/mnt/buffer/tuwenming/checkpoints/VocalNet/qwen25-7b-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder/checkpoint-1000"        # VocalNet模型路径，需要根据实际情况填写
+export VOCALNET_MODEL="/mnt/buffer/tuwenming/checkpoints/VocalNet/qwen25-7b-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder/checkpoint-4000"        # VocalNet模型路径，需要根据实际情况填写
 export COSYVOICE_MODEL="/share/nlp/tuwenming/models/CosyVoice/CosyVoice2-0.5B-old"       # CosyVoice2-0.5B模型路径，需要根据实际情况填写
 export PROMPT_SPEECH="/share/nlp/tuwenming/projects/URO-Bench/examples/VocalNet-test/omni_speech/infer/alloy.wav"
 
 # code dir
 model_name=VocalNet
 code_dir="/share/nlp/tuwenming/projects/URO-Bench"                           # URO-Bench代码目录，需要根据实际情况填写
-log_dir="/share/nlp/tuwenming/projects/UltraVoice_dev/eval/URO-Bench/VocalNet-Qwen25-7B-UltraVoice100k-Clean-Steps1000-SFT"     # 评估结果保存目录，需要根据实际情况填写
+log_dir="/share/nlp/tuwenming/projects/UltraVoice_dev/eval/URO-Bench/VocalNet-Qwen25-7B-UltraVoice100k-Clean-Steps4000-SFT"     # 评估结果保存目录，需要根据实际情况填写
 whisper_dir="/share/nlp/tuwenming/models/openai/whisper-large-v3"                 # whisper模型路径，需要根据实际情况填写
 uro_data_dir="/share/nlp/tuwenming/projects/URO-Bench/URO-Bench-data"                  # URO-Bench数据目录，需要根据实际情况填写
 
@@ -205,4 +205,4 @@ done
 # echo "评估完成！"
 
 
-# nohup bash ./scripts/vocalnet-asr-eval-qwen7b-sft.sh > /share/nlp/tuwenming/projects/UltraVoice_dev/logs/VocalNet-Qwen25-7B-SFT-eval-$(date +%Y%m%d%H%M%S).log 2>&1 &
+# nohup bash ./scripts/vocalnet-asr-eval-qwen7b-sft-3.sh > /share/nlp/tuwenming/projects/UltraVoice_dev/logs/VocalNet-Qwen25-7B-SFT-eval-$(date +%Y%m%d%H%M%S).log 2>&1 &
